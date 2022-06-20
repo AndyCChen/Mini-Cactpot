3de0fe730cd95e04d53555421ba4810d5e1dc135
Andy Chen <acchen@cpp.edu>
2022-06-20 13:44:30 -0700

    created columns showing payout rewards

---
+++ src/components/Payout.js
@@ -0,0 +1,14 @@
+import PayoutColumns from "./PayoutColumns";
+
+const Payout = () => {
+   return (
+      <div>
+         <header className='Payout-Header'>Payout</header>
+         <PayoutColumns />
+      </div>
+   )
+}
+
+Payout.propTypes = {}
+
+export default Payout
