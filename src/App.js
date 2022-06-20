cf70612ff09946c707d25e072bc8ee6c91780dd0
Andy Chen <acchen@cpp.edu>
2022-06-20 13:56:17 -0700

    added gameBoard and Payout components to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Payout from './components/Payout';
+import GameBoard from './components/GameBoard';
+
 function App() {
 	return (
 		<div className="GameArea-Container">
 			<header className="Header">
 				MINI CACTPOT
 			</header>
+			<GameBoard />
+			<Payout />
 		</div>
 	);
 }
