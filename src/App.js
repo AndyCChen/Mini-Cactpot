6c5cfa72cf890d7bafd717a1d4dd13a5746368b3
Andy Chen <acchen@cpp.edu>
2022-06-26 15:34:16 -0700

    final-ish commit? added github link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Payout from './components/Payout';
 import GameBoard from './components/GameBoard';
+import { BiLinkExternal } from 'react-icons/bi';
 
 function App() {
 	return (
-		<div className="GameArea-Container">
-			<header className="Header">
-				MINI CACTPOT
-			</header>
-			<GameBoard />
-			<Payout />
+		<div>
+			<a className='github-button' href='https://github.com/AndyCChen/Mini-Cactpot' target='_blank'>
+				GitHub
+				<BiLinkExternal size={20}/>
+			</a>
+			<div className="GameArea-Container">
+				<header className="Header">
+					MINI CACTPOT
+				</header>
+				<GameBoard />
+				<Payout />
+			</div>
 		</div>
 	);
 }
